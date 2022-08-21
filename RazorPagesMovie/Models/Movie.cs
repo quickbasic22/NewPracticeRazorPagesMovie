5b2f11cb4942f37781a69f8ffa3541364cfebb38
David Morrow <quickbasic44@outlook.com>
2022-08-20 20:25:29 -0400

    Add project files.

---
+++ RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; } = string.Empty;
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; } = string.Empty;
+        [Column(TypeName = "decimal(18, 2)")]
+
+        public decimal Price { get; set; }
+    }
+}
---
+++ RazorPagesMovie/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RazorPagesMovie.Data;
+using RazorPagesMovie.Models;
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddDbContext<RazorPagesMovieContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("RazorPagesMovieContext") ?? throw new InvalidOperationException("Connection string 'RazorPagesMovieContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
